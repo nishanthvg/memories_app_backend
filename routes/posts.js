53b92dbdd177321c2592f934c642608cf11964ac
nishanthvg <nishanthvg33@gmail.com>
2021-11-12 22:48:14 +0530

    part5 done: comments implemented

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {getPost,getPosts, createPost, updatePost, deletePost, likePost, getPostBySearch} from '../controllers/posts.js'
+import {commentPost,getPost,getPosts, createPost, updatePost, deletePost, likePost, getPostBySearch} from '../controllers/posts.js'
 import {auth} from "../middleware/auth.js"
 const router = express.Router()
 // - /posts/..
@@ -8,6 +8,8 @@ router.get('/search', getPostBySearch)
 router.get('/:id', getPost)
 
 router.post('/', auth,createPost)
+router.post('/:id/commentPost',auth,commentPost)
+
 router.patch('/:id', auth, updatePost)
 router.delete("/:id", auth, deletePost)
 router.patch('/:id/likePost', auth,likePost)
